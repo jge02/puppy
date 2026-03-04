8d00f13712ea190653b2c9c209c8222983149d23
UBC Student <gejunming826@gmail.com>
2026-03-04 05:43:40 -0800

    Allow xiaogou.ink in CORS

--- app/main.py
+++ app/main.py
@@ -30,6 +30,11 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://18.219.24.174",
+        "http://xiaogou.ink",
+        "https://xiaogou.ink",
+        "http://www.xiaogou.ink",
+        "https://www.xiaogou.ink",
     ],
     allow_credentials=True,
     allow_methods=["*"],
